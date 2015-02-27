20ea6520efb5c56a840f3666e09b7a1207f29474
Robert Ansel <robert.ansel@gmail.com>
2015-02-27 19:22:46 +0000

    initial sketch

---
+++ Proj_2/node.c
@@ -0,0 +1,22 @@
+
+
+
+/* Notes:
+    rip algorithm cases: http://en.wikipedia.org/wiki/Routing_Information_Protocol
+    for listening on multiple fd/sockets's simultaneously use fd_set and select() 
+
+*/
+
+int main(int argc, char ** argv) {
+    // Initialize based on input file
+    // setup non-blocking UDP recieve socket
+    // setup non-blocking UDP send socket
+    // initialize routing information
+    while (1) {
+    	// check for user input
+    		// handle
+    	// check for recieved packet
+    		// handle
+    }
+
+}
